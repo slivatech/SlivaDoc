7070e05d9c84e39e4b830f731060d6891a44814f
madbull12 <huangandrian02@gmail.com>
2023-03-20 21:38:10 +0700

    test

--- src/components/Button/ButtonStyle.tsx
+++ src/components/Button/ButtonStyle.tsx
@@ -1,10 +1,15 @@
 import styled from "styled-components";
 
-export const StyledButton = styled.button<{ borderRadius:string,bgColor:string,color:string,size:"normal"|"large" }>`
-    background:${props => props.bgColor};
-    color:${props => props.color};
-    padding:8px 24px;
-    border:none;
-    border-radius: ${props=>props.borderRadius || "4px"};
-   cursor: pointer;
-`
+export const StyledButton = styled.button<{
+  borderRadius: string;
+  bgColor: string;
+  color: string;
+  size: "normal" | "large";
+}>`
+  background: ${(props) => props.bgColor};
+  color: ${(props) => props.color};
+  padding: 8px 24px;
+  border: none;
+  border-radius: ${(props) => props.borderRadius || "4px"};
+  cursor: pointer;
+`;
