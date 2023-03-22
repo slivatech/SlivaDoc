d18c763f0196ae8ce540b719ad9b8c68bf6c301d
evans.moris21 <evans.moris21@gmail.com>
2023-03-22 15:17:11 +0700

    first pull

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
 import React from "react";
 import { ThemeProvider } from "styled-components";
 import logo from "./logo.svg";
 import HomePage from "./pages/HomePage";
 import GlobalCss from "./styles/Global";
 import { defaultTheme } from "./styles/Theme";
-=======
->>>>>>> 5faa1f0f97354369bdfc9450239afe2b753980a4
 
 import './App.css';
 import DetailArtikel from './pages/DetailArtikel/DetailArtikel';
@@ -14,14 +11,10 @@ import DetailArtikel from './pages/DetailArtikel/DetailArtikel';
 
 const App = () => {
   return (
-<<<<<<< HEAD
     <ThemeProvider theme={defaultTheme}>
       <GlobalCss />
       <HomePage />
     </ThemeProvider>
-=======
-    <DetailArtikel/>
->>>>>>> 5faa1f0f97354369bdfc9450239afe2b753980a4
   );
 }
 
