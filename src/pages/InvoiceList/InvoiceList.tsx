25d6baa31513e5a4cd34c74f3eb1971f18caa5cf
fiqihaissyyah <fiqihaissyyah@gmail.com>
2023-04-07 13:46:45 +0700

    add table with react table

--- src/pages/InvoiceList/InvoiceList.tsx
+++ src/pages/InvoiceList/InvoiceList.tsx
@@ -1,11 +1,13 @@
-import React, {SetStateAction, useState, Dispatch, useMemo} from 'react'
+import React, {SetStateAction, useState, Dispatch, useMemo, useEffect} from 'react'
 import ava from '../../assets/doctor.png'
 import moneyIc from '../../assets/icon/payment.svg'
 import calenderIC from '../../assets/icon/Calendar.svg'
+import editIc from '../../assets/icon/edit.svg'
 import { Container } from '../PageArtikel/PageArtikelStyle'
-import { TableCustom } from './InvoiceListStyle'
+import {  TableRow, TableHead, TableStyle } from './InvoiceListStyle'
 import { customers } from './InvoiceListData'
-import { Cell, Column, useSortBy, useTable } from "react-table";
+import { Column, useSortBy, useTable, useRowSelect, Cell } from "react-table";
+import Checkbox from './Checkbox'
 
 export type TableProps = {
     data: Array<any>;
@@ -15,10 +17,11 @@ export type TableProps = {
 };
 
 interface ColumnDetails {
-    [key: string]: string;
+    // [id: number]: any;
+    [key: string]: any;
 };
+
 const InvoiceList = () => {
-    
     const data = useMemo<ColumnDetails[]>(() => customers, []);
     const columns:any = useMemo(
         () => [
@@ -45,83 +48,116 @@ const InvoiceList = () => {
     ], []
     );
 
-    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable({ columns, data }, useSortBy);
+    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow, selectedFlatRows } =
+    useTable({ columns, data }, useSortBy, 
+        // useRowSelect,
+        // (hooks) => {
+        //     hooks.visibleColumns.push(() => {
+        //         return [
+        //             {
+        //                 id: 'selection',
+        //                 Header: ({getToggleAllRowsSelectedProps}) => (
+        //                     <Checkbox {...getToggleAllRowsSelectedProps}/>
+        //                 ),
+        //                 Row: ({getToggleRowSelectedProps}) => (
+        //                     <Checkbox {...getToggleRowSelectedProps}/> 
+        //                 )
+        //             }
+        //         ]
+        //     })
+        // }
+        );
+    
+    const [editable, setEditable] = useState(false);
+
 
   
-  return (
-    <>
-    <Container>
-        <div {...getTableProps()}>
-            <div>
-              {headerGroups.map((headerGroup, i) => (
-                <tr {...headerGroup.getHeaderGroupProps()}>
-                  {headerGroup.headers.map((column) => (
-                    <th
-                      {...column.getHeaderProps(column.getSortByToggleProps())}
-                    >
-                      {column.render("Header")}
-                      {column.isSorted ? (column.isSortedDesc ? " ▼" : " ▲") : ""}
-                      {/* <img
-                        src="/assets/arrow_dropdown2.svg"
-                        className={column.isSortedDesc ? "desc" : ""}
-                      /> */}
-                    </th>
-                  ))}
-                </tr>
-              ))}
-            </div>
-            <tbody {...getTableBodyProps()}>
-              <tr></tr>
-            </tbody>
-        </div>
-    </Container>
-    </>
-  )
+    return (
+        <>
+        <Container>
+            <TableStyle {...getTableProps()}>
+                <TableHead>
+                {headerGroups.map((headerGroup, i) => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                        <th> <input type='checkbox' /> </th>
+                        {headerGroup.headers.map((column) => (
+                            <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                            {column.render("Header")}
+                            {column.isSorted ? (column.isSortedDesc ? " ▼" : " ▲") : ""}
+                            </th>
+                        ))}
+                        <th> <button className='btn-edit'><img src={editIc}/></button> </th>
+                    </tr>
+                ))}
+                </TableHead>
+                <tbody {...getTableBodyProps()}>
+
+                {rows.map((row, id) => {
+                    prepareRow(row);
+                    const {date, invoiceId, name, payment, status} = row.values;
+                    return (
+                        <TableRow {...row.getRowProps()}>
+                            <td><input type='checkbox'/></td>
+                            <td>{invoiceId}</td>
+                            <td>
+                                <div className='name'>
+                                    <img src={ava}/>
+                                    <div>{name}</div>
+                                </div>
+                            </td>
+                            <td>
+                                <div className='list-icon'>
+                                    <img src={moneyIc}/>
+                                    <div>{payment}</div>
+                                </div>
+                            </td>
+                            <td>
+                                <div className='list-icon'>
+                                    <img src={calenderIC}/>
+                                    <div> {date}</div>
+                                </div>
+                            </td>
+                            <td> <button className={`${status.toLowerCase()} btn-status`}>{status}</button> </td>
+                            <td> 
+                                <button className='btn-edit' key={id} onClick = {()=> setEditable(prev => !prev)}>
+                                    <img src={editIc} />
+                                </button>
+                                {
+                                    editable  ? 
+                                    <span>
+                                        <button style={{display : "block"}}>Complete</button> 
+                                        <button style={{display : "block"}}>Cancel</button> 
+                                        <button style={{display : "block"}}>Pending</button> 
+                                    </span>  : " "
+                                }
+                            </td>
+                        </TableRow>
+                    );
+                })}
+                </tbody>
+            </TableStyle>
+        </Container>
+        </>
+    )
 }
+// const TableContent = (cell: {value: value<ReactNodecolumn, any>; cell: Cell<ColumnDetails, any> }) => {
+//     console.log(cell);
+//     return (
+//       <TableData>
+//         {cell.column.id === "name" ? (
+//           <div style={{ display: "flex", alignItems: "center", gap: ".5rem" }}>
+//             <img src={cell.row.original.image} />
+//             {cell.value}
+//           </div>
+//         ) : cell.column.id === "gender" ? (
+//           <div className={`${cell.value.toLowerCase()} genderRow`}>
+//             {cell.value}
+//           </div>
+//         ) : (
+//           cell.value
+//         )}
+//       </TableData>
+//     );
+//   };
 
 export default InvoiceList
-
-{/* <thead>
-                <tr>
-                    <th>
-                        <input type="checkbox" name="select-all" />
-                    </th>
-                    <th>Invoice Id</th>
-                    <th>Name</th>
-                    <th>Total Price</th>
-                    <th>Date</th>
-                    <th>status</th>
-                </tr>
-            </thead>
-            <tbody>
-                    <div className='row'>
-                <tr>
-                        <td>
-                            <input type='checkbox'/>
-                        </td>
-                        <td>#876364</td>
-                        <td>
-                            <div className='name'>
-                                <img src={ava}/>
-                                <div>Arrora gaur</div>
-                            </div>
-                        </td>
-                        <td>
-                            <div className='list-icon'>
-                                <img src={moneyIc}/>
-                                <div>Rp.1.500.000</div>
-                            </div>
-                        </td>
-                        <td>
-                            <div className='list-icon'>
-                                <img src={calenderIC}/>
-                                <div> 12 Dec, 2020</div>
-                            </div>
-                        </td>
-                        <td>
-                            <button>Diproses</button>
-                        </td>
-                </tr>
-                </div>
-            </tbody> */}
--- src/pages/InvoiceList/InvoiceListStyle.tsx
+++ src/pages/InvoiceList/InvoiceListStyle.tsx
@@ -1,34 +1,112 @@
 import styled from "styled-components";
 
-export const TableCustom = styled.div`
-    width: 100%;
-    display: grid;
-    grid-template-columns: 50px 100px repeat(auto-fit, minmax(125px, 1fr));
-    gap: 1rem;
 
-    .row {
-        background-color: #fff;
-        border-radius: 10px;
-        padding: 10px;
+export const TableStyle = styled.table`
+  width: 100%;
+  border-collapse: separate;
+  border-spacing: 0 0.5rem;
+  margin: 0 auto;
+  td:first-child {
+    border-top-left-radius: 10px;
+    border-bottom-left-radius: 10px;
+  }
+  td:last-child {
+    border-bottom-right-radius: 10px;
+    border-top-right-radius: 10px;
+  }
+
+  button {
+    border: none;
+    cursor: pointer;
+  }
+
+  
+`;
+
+export const TableHead = styled.thead`
+  color: #06152b;
+  font-size: 12px;
+    th {
+      text-align: start;
+      cursor:pointer;
     }
+    th:first-child {
+        padding-left: 2rem;
+    }
+    th:last-child {
+        text-align: end;
+        padding-right: 2rem;
+    }
+    img {
+        margin-left: 0.5rem;
+    }
+    .desc {
+        transform: rotateX(3.142rad);
+    }
+    .btn-edit {
+        background-color: #FAFAFA;
+        img {
+            background-color: #FAFAFA;
+        }
+    }
+    
+`;
 
+export const TableRow = styled.tr`
+  font-size: 14px;
+  background-color: #fff;
+  height: 70px;
+    td:first-child {
+        padding-left: 2rem;
+    }
+    td:last-child {
+        text-align: end;
+        padding-right: 2rem;
+    }
+    td:nth-last-child(2) {
+        width: 200px;
+    }
     .name {
         display: flex;
         align-items: center;
         column-gap: 15px;
-        
+    
         img {
             width: 36px;
             height: 36px;
             border-radius: 50%;
             object-fit: cover;
         }
-    };
-
+    }
     .list-icon {
         display: flex;
         align-items: center;
         column-gap: 10px;
     }
+    .btn-edit {
+        background-color: white;
+        img {
+            background-color: white;
+        }
+    }
 
+    .btn-status {
+        padding: 15px 44px;
+        min-width: 150px;
+        border-radius: 22.5px;
+    }
+    .complete {
+        background-color: rgba(3, 168, 158, 0.1);
+        color: #03A89E;
+    }
+    .pending {
+        background-color: rgba(58, 54, 219, 0.1);
+        color: #3A36DB;;
+    }
+    .cancel {
+        background-color: rgba(255, 105, 180, 0.1);
+        color: #FF69B4;;
+    }
 `
+
+
